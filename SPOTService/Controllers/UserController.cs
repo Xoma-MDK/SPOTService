63406c3181745c8512819bb67d85e1138a696a63
Daniil Molotkov <dmolotkov@rit-it.com>
2024-01-03 14:09:18 +0700

    feat: add UserController

---
+++ SPOTService/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPOTService.Controllers
+{
+    public class UserController(ILogger<UserController> logger) : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger = logger;
+    }
+}
