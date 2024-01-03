4aa172c75df877926a45b02f6ebb7e34d49c4670
Daniil Molotkov <dmolotkov@rit-it.com>
2024-01-03 23:25:23 +0700

    fix: fix FK Role

---
+++ SPOTService/Migrations/20240103151426_FixRoleKey.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPOTService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoleKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_RoleId",
+                table: "User");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleId",
+                table: "User",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_RoleId",
+                table: "User");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleId",
+                table: "User",
+                column: "RoleId",
+                unique: true);
+        }
+    }
+}
