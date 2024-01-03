707061b8dab439b83f0e80e4a4a67fbebd1981ac
Daniil Molotkov <dmolotkov@rit-it.com>
2024-01-03 14:06:12 +0700

    feat: add model

---
+++ SPOTService/Infrastructure/InternalServices/Auth/Models/TokensResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SPOTService.Infrastructure.InternalServices.Auth.Models
+{
+    public class TokensResponse
+    {
+        public string? Access;
+        public string? Refresh;
+    }
+}
